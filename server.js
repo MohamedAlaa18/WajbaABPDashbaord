4e3cb4bb6065f71b95474cfb8f4fa3190f5ca01b
Haitham AbdElRady <haithamshaabann@gmail.com>
2025-02-10 19:45:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server//
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Wajba'));
+app.use(express.static(__dirname + '/dist/build'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Wajba/index.html'));
+res.sendFile(path.join(__dirname+'/dist/build/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
