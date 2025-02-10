013849f23254e0fca13b11ccc0fab0a345eeb782
Haitham AbdElRady <haithamshaabann@gmail.com>
2025-02-10 19:01:22 +0200

    Update server.js for pdoduction 6

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 
-const buildPath = path.join(__dirname, 'dist', 'Wajba');
-console.log('Serving static files from:', buildPath);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Wajba'));
 
-fs.readdir(buildPath, (err, files) => {
-  if (err) {
-    console.error('Directory not found:', err);
-  } else {
-    console.log('Directory contents:', files);
-  }
-});
-
-// Serve only the static files from the dist directory
-app.use(express.static(buildPath));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(buildPath, 'index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/Wajba/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 8080);
